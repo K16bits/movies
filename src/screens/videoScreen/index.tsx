253ac706e5b1c709d992ad10f91b515b2d88a2f0
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-07-01 17:09:43 -0300

    controle de video

--- src/screens/videoScreen/index.tsx
+++ src/screens/videoScreen/index.tsx
@@ -1,21 +1,87 @@
 import React from 'react';
-import { View, Text, } from 'react-native';
+import { View, Text,TouchableOpacity} from 'react-native';
 import { Video } from 'expo-av';
+import { useState } from 'react';
 
 import { styles } from './styles'
+import { 
+    SimpleLineIcons,
+    FontAwesome,
+    Feather,
+} from '@expo/vector-icons'
 
 export function videoScreen() {
     const videoURL = "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"
+    const [volume,setVolume] = useState(0.7);
+    const [playState,setPlayState] = useState(false);
+
+    function handlePlay(){
+        setPlayState(!playState)
+    }
+
+    function handleVolumePlus(){
+        if(volume !== 1.0){
+            const valor = Number((volume+0.1).toFixed(2))
+            setVolume(valor)
+        }
+        console.log(volume)
+    }
+
+    function handleVolumeSub(){
+        if(volume !== 0.0){
+            const valor = Number((volume-0.1).toFixed(2))
+            setVolume(valor)
+            }
+        console.log(volume)
+    }
+
     return (
         <View style={styles.container}>
-                <Text style={styles.title}>Video.mp4</Text>
-                    <Video 
-                        style={styles.video}
-                        source={{ uri: videoURL }}
-                        useNativeControls
-                        resizeMode="contain"
+            <View style={styles.videoContainer}>
+            <Text style={styles.title}>Video.mp4</Text>
+                <Video
+                    style={styles.video}
+                    resizeMode='contain'
+                    source={{ uri: videoURL }}
+                    shouldPlay={playState}
+                    volume={volume}
                     />
-     
+            </View>
+            <View style={styles.buttonsContainer}>
+                    <TouchableOpacity onPress={()=>{
+                        handlePlay()
+                    }}>
+                        {
+                            playState?
+                            <SimpleLineIcons name='control-pause' style={styles.icon}/>
+                            :<FontAwesome name='play' style={styles.icon} />
+                        }
+                        
+                    </TouchableOpacity>
+                    {
+                        volume == 0.0 ?
+                        <Feather name="volume-x" style={styles.icon}/>
+                        :
+                        <TouchableOpacity onPress={()=>{
+                            console.log('menos')
+                            handleVolumeSub()
+                        }}>
+                            <SimpleLineIcons 
+                                name='volume-1'
+                                style={styles.icon}
+                                />
+                        </TouchableOpacity>
+                    }
+                    <TouchableOpacity onPress={()=>{
+                        handleVolumePlus()
+                    }}>
+                        <SimpleLineIcons 
+                            name='volume-2'
+                            style={styles.icon}
+                            />
+                    </TouchableOpacity>
+            </View>
         </View>
+        
     )
 }
--- src/screens/videoScreen/styles.ts
+++ src/screens/videoScreen/styles.ts
@@ -1,21 +1,38 @@
 import {StyleSheet,Dimensions} from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 import { theme } from '../../theme/theme';
 
-const {height} = Dimensions.get('screen')
+const {height,width} = Dimensions.get('screen')
 export const styles = StyleSheet.create({
     container:{
         flex: 1,
+        height: height,
         backgroundColor:theme.color.background,
-        justifyContent:'center',
-        alignItems:'center'
+    },
+    videoContainer:{
+        marginTop:getStatusBarHeight()+25,
+        alignItems:'center',
+        justifyContent:'center'
     },
     video:{
-        width: '100%',
+        width: 200,
         height: 200,
     },
     title:{
         fontFamily:theme.fonts.title,
         fontSize:18,
         color: 'white',
+    },
+    buttonsContainer:{
+        flexDirection:'row',
+        justifyContent:'center'
+    },
+    icon:{
+        color:'white',
+        fontSize:28,
+        borderWidth:1,
+        borderColor:'white',
+        padding: 10,
+        marginLeft:10
     }
 })
