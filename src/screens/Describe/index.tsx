8dcd76acf42664e5f89a37bfec5d57047cb5c6da
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-07-04 15:57:24 -0300

    loading na tela com boolean

--- src/screens/Describe/index.tsx
+++ src/screens/Describe/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { useRoute } from '@react-navigation/native';
 import { View, Text, Image } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+
+import {Loading} from '../../components/Loading'
 
 import { api } from '../../services/api';
 import { styles } from './styles';
@@ -11,11 +13,13 @@ const { API_URL } = process.env;
 export function Describe() {
     const router = useRoute()
     const [movieData, setMovieData] = useState('')
+    const [loadState,setLoadState] = useState(true)
 
     async function getDataID() {
         const { itemID,category } = router.params
         const response = await api.get(`https://api.themoviedb.org/3/${category}/${itemID}?api_key=${API_URL}&language=pt-BR`)
         setMovieData(response.data)
+        setLoadState(false)
     }
 
     useEffect(() => {
@@ -25,7 +29,9 @@ export function Describe() {
     const urlPoster = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${movieData.poster_path}`
     return (
         <View style={styles.container}>
-            <View style={styles.content}>
+            {
+                loadState ?  <Loading/> :
+                <View style={styles.content}>
                 <Image
                     source={{ uri: urlPoster }}
                     style={styles.image}
@@ -35,9 +41,8 @@ export function Describe() {
                 <View style={styles.contentText}>
                     <Text style={styles.text}>{movieData.overview}</Text>
                 </View>
-
             </View>
-
+            }
         </View>
     )
 }
