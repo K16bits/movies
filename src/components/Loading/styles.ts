a3a6f932412bdf8f40dd45ae14cfc7c2781269ad
Cristiano Lima <52179531+K16bits@users.noreply.github.com>
2021-07-04 15:52:36 -0300

    componente de carregamento

---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      color: 'red',
+      justifyContent:'center',
+      alignItems:'center'
+    }
+})
